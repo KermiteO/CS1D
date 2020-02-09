0c53f14a385972aa54e5777f1b37cda83ae32380
KermiteO <kermicheo@gmail.com>
2020-02-08 21:07:35 -0800

    Add files via upload

---
+++ CS1D - Assignment 6 - Heap Sort/Header.h
@@ -0,0 +1,25 @@
+/*
+ * Header.h
+ *
+ *  Created on: Feb 13, 2017
+ *      Author: Kermite
+ */
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<sstream>
+using namespace std;
+
+string PrintHeader(char   prType,
+				   string programmer,
+				   int	  prNum,
+				   string prName,
+				   string prompt);
+
+
+
+#endif /* HEADER_H_ */
